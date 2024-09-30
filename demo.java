f637ddf24181fd5b12479162c9280e04f0682076
aishuu5756 <paishu139@gmail.com>
2024-09-30 15:18:38 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner
+  {
+    class Add
+      {
+        public static void main(String args[])
+        int num=10;
+        num=num+1;
+System.out.println("num is"+num);
+}
+}
